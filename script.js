3491c835e996095acfb6fcea151d0e365055fcc7
Avanish Singh <107432071+avanishsinghav@users.noreply.github.com>
2025-02-18 20:10:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 function createTable() {
     //Write your code here
-  
+    let rows = prompt("Input number of rows");
+	let cols = prompt("Input number of column");
+
+	rows = parseInt(rows);
+	cols = parseInt(cols);
+
+	if(isNaN(rows) || isNaN(cols) || rows <=0 || cols <= 0){
+		alert("please enter valid positive numbers for rows and columns");
+		return;
+	}
+	let table = document.getElementById("myTable");
+	table.innerHTML = "";
+
+	for(let i = 0; i<rows; i++){
+		let row = table.insertRow();
+		for(let j = 0; j<cols; j++){
+			let cell = row.insertCell();
+			cell.textContent = `Row-${i} column-${j}`;
+		}
+	}
+	
 }
